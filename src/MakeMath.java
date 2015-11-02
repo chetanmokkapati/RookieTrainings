03f9694ee22963c2d3b8745d7a6136cae5310476
Chetan Mokkapati <cmokkapati123@gmail.com>
2015-11-02 15:16:40 -0800

    Finish MakeMath.java

--- src/MakeMath.java
+++ src/MakeMath.java
@@ -2,6 +2,11 @@
 public class MakeMath {
 	public static void main(String []args) {
 		// create an array with 4 indexes
+		int myArray[] = new int[4];
+		for (int i=0; i<myArray.length;i++){
+			myArray[i] = i;
+			System.out.println(myArray[i] + "");
+		}
 		
 		// create a for loop that while i is less than the 
 		// yourArray.length, then it will set the element to i at 
@@ -9,18 +14,36 @@ public class MakeMath {
 		// During this, print your array
 		
 		System.out.println("onto next");
-		
+		for(int i=0; i<myArray.length; i++){
+			myArray[i] = i + 1;
+			System.out.println(myArray[i] + "");
+		}
 		// for each in yourArray, add 1
 		// During this, print yourArray
 		
 		System.out.println("onto next");
-		
+		int x=0;
+		for (int i=0; i<myArray.length; i++){
+			if( x<myArray[i] ){
+				System.out.println(x + "");
+				x=x+1;
+		}
+		}
 		// create an integer x with value 0
 		// while x is less than yourArray, add one to x
 		// during this, print x
 		
 		System.out.println("onto next");
-		
+		for(int i=0; i<myArray.length; i++){
+			if(myArray[i]<0)
+				System.out.println("blah");
+			else if(myArray[i]<1)
+				System.out.println("bleh");
+			else if(myArray[i]<2)
+				System.out.println("blooh");
+			else
+				System.out.println("raaaaawr");
+		}
 		// for each element in yourArray
 		// if each is less than 0 print blah
 		// else if each is less than 1 print bleh
